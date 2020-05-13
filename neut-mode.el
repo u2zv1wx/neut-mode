7914aa180c1130432366bd450fafa837a400f7b4
u2zv1wx <u2zv1wx@protonmail.ch>
2020-05-13 10:00:55 +0900

    cleanup

--- neut-mode.el
+++ neut-mode.el
@@ -21,16 +21,15 @@
      (modify-syntax-entry ?\n ">" syntax-table)
      (modify-syntax-entry ?? "w" syntax-table)
      (modify-syntax-entry ?. "w" syntax-table)
-     (modify-syntax-entry ?@ "w" syntax-table)
      syntax-table))
   (setq font-lock-defaults
         `(,`((,(regexp-opt '("array" "arrow" "enum" "exists" "forall" "hom" "pi" "product" "sigma" "struct" "tau" "type" "universe" "Π" "∑") 'symbols)
              . font-lock-type-face)
-            (,(regexp-opt '("apply" "array-elimination" "array-introduction" "assume" "begin" "construct" "corollary" "default" "define" "destruct" "ensure" "enum-elimination" "enum-introduction" "erase" "fix" "if" "include" "inductive" "irreducible" "lambda" "lemma" "let" "notation" "constant" "pi-elimination" "pi-introduction" "proposition" "record" "section" "end" "sigma-elimination" "sigma-introduction" "statement" "struct-elimination" "struct-introduction" "switch" "theorem" "tuple" "unuse" "use" "with" "witness" "λ") 'symbols)
+            (,(regexp-opt '("apply" "array-elimination" "array-introduction" "assume" "construct" "corollary" "default" "define" "destruct" "end" "ensure" "enum-elimination" "enum-introduction" "erase" "fix" "if" "include" "inductive" "irreducible" "lambda" "lemma" "let" "notation" "constant" "pi-elimination" "pi-introduction" "proposition" "record" "section" "sigma-elimination" "sigma-introduction" "statement" "struct-elimination" "struct-introduction" "switch" "theorem" "tuple" "unuse" "use" "with" "witness" "λ") 'symbols)
              . font-lock-keyword-face)
             (,(regexp-opt '("introspect" "question" "splice") 'symbols)
              . font-lock-builtin-face)
-            (,(regexp-opt '("*" "?admit" "_" "admit" "unsafe") 'symbols)
+            (,(regexp-opt '("*" "?admit" "_" "admit") 'symbols)
              . font-lock-warning-face)
             ))))
 
